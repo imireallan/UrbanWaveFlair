9189ccc5a2ffa02afa516b79599e9f87a597b293
Allan Imire <allanimire@gmail.com>
2023-09-28 06:33:31 +0300

    add custom tagged manager class

--- urbanwaveflair/tags/models.py
+++ urbanwaveflair/tags/models.py
@@ -7,8 +7,17 @@ class Tag(models.Model):
     label = models.CharField(max_length=255)
 
 
+class TaggedItemManager(models.Manager):
+    def get_tags_for(self, obj_type: models.Model, obj_id: int):
+        content_type = ContentType.objects.get_for_model(obj_type)
+        return self.select_related("tag").filter(
+            content_type=content_type, object_id=obj_id
+        )
+
+
 class TaggedItem(models.Model):
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
+    objects = TaggedItemManager()
