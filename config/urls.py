de181009ef1df8322742e5fd0386ea4fead932b9
Allan Imire <allanimire@gmail.com>
2023-09-28 06:40:08 +0300

    rename django admin site header and title

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "Urbanwaveflair Admin"
+admin.site.site_title = "Admin"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("playground/", include("urbanwaveflair.playground.urls")),
