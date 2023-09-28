8aa98a891a4e49726c8312dccbe2e9253974d0a2
Allan Imire <allanimire@gmail.com>
2023-09-28 10:52:42 +0300

    modify models

--- urbanwaveflair/tags/models.py
+++ urbanwaveflair/tags/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Tag(models.Model):
     label = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.label
+
 
 class TaggedItemManager(models.Manager):
     def get_tags_for(self, obj_type: models.Model, obj_id: int):
