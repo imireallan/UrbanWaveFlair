ba802e2f8abbd3341419d08f8a0363219754f021
Allan Imire <allanimire@gmail.com>
2023-09-29 19:21:38 +0300

    add filter backends

--- urbanwaveflair/store/views.py
+++ urbanwaveflair/store/views.py
@@ -1,15 +1,27 @@
 from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
+from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from urbanwaveflair.store.models import Collection, Product
-from urbanwaveflair.store.serializers import CollectionSerializer, ProductSerializer
+from urbanwaveflair.store.models import Collection, Product, Review
+from urbanwaveflair.store.pagination import DefaultPagination
+from urbanwaveflair.store.serializers import (
+    CollectionSerializer,
+    ProductSerializer,
+    ReviewSerializer,
+)
 
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ["collection_id"]
+    search_fields = ["title", "description"]
+    ordering_fields = ["unit_price", "last_update"]
+    pagination_class = DefaultPagination
 
     def destroy(self, request, *args, **kwargs):
         product = self.get_object()
@@ -37,3 +49,15 @@ class CollectionViewSet(ModelViewSet):
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
         return super().destroy(request, *args, **kwargs)
+
+
+class ReviewViewSet(ModelViewSet):
+    serializer_class = ReviewSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"product_pk": self.kwargs.get("product_pk")})
+        return context
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs["product_pk"])
