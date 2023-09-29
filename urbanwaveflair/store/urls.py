7989315f4f9a7e285f53a5680753dbf3b9d79741
Allan Imire <allanimire@gmail.com>
2023-09-29 17:54:49 +0300

    add router for viewsets

--- urbanwaveflair/store/urls.py
+++ urbanwaveflair/store/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from urbanwaveflair.store import views
 
-urlpatterns = [
-    path("products/", views.ProductList.as_view()),
-    path("products/<int:pk>", views.ProductDetail.as_view()),
-    path("collections/", views.CollectionList.as_view()),
-    path("collections/<int:pk>", views.CollectionDetail.as_view()),
-]
+router = DefaultRouter()
+router.register("products", viewset=views.ProductViewSet)
+router.register("collections", viewset=views.CollectionViewSet)
+
+urlpatterns = router.urls
