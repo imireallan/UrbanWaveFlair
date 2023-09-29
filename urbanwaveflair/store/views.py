0c7415f0d41c3be63daf89e4df2a7699bb19a786
Allan Imire <allanimire@gmail.com>
2023-09-29 14:12:02 +0300

    add product and collection function based views

---
+++ urbanwaveflair/store/views.py
@@ -0,0 +1,84 @@
+from django.db.models import Count
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from urbanwaveflair.store.models import Collection, Product
+from urbanwaveflair.store.serializers import CollectionSerializer, ProductSerializer
+
+
+@api_view(["GET", "POST"])
+def products_list(request):
+    if request.method == "GET":
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(
+            queryset, many=True, context={"request": request}
+        )
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == "GET":
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        if product.orderitems.count() > 0:
+            return Response(
+                {
+                    "error": "This product cannot be deleted because it inclueds one or more order items"
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(["GET", "POST"])
+def collections_list(request):
+    if request.method == "GET":
+        queryset = Collection.objects.annotate(products_count=Count("products"))
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(["GET", "PUT", "DELETE"])
+def collection_detail(request, pk):
+    collection = get_object_or_404(
+        Collection.objects.annotate(products_count=Count("products")), pk=pk
+    )
+    if request.method == "GET":
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CollectionSerializer(collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    if request.method == "DELETE":
+        if collection.products.count() > 0:
+            return Response(
+                {
+                    "error": "This collection cannot be deleted because it includes on or more product."
+                },
+                status=status.HTTP_405_METHOD_NOT_ALLOWED,
+            )
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
