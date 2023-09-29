4e099b6327259db6972c68a2063d6669dd329f9d
Allan Imire <allanimire@gmail.com>
2023-09-29 15:36:26 +0300

    add generic views

--- urbanwaveflair/store/views.py
+++ urbanwaveflair/store/views.py
@@ -1,44 +1,27 @@
 from django.db.models import Count
-from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
 
 from urbanwaveflair.store.models import Collection, Product
 from urbanwaveflair.store.serializers import CollectionSerializer, ProductSerializer
 
 
-@api_view(["GET", "POST"])
-def products_list(request):
-    if request.method == "GET":
-        queryset = Product.objects.select_related("collection").all()
-        serializer = ProductSerializer(
-            queryset, many=True, context={"request": request}
-        )
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class ProductList(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def product_detail(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == "GET":
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == "DELETE":
+class ProductDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+    def delete(self, request):
+        product = self.get_object()
         if product.orderitems.count() > 0:
             return Response(
                 {
-                    "error": "This product cannot be deleted because it inclueds one or more order items"
+                    "error": "This product cannot be deleted because it includes one or more order items"
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
@@ -46,33 +29,17 @@ def product_detail(request, id):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(["GET", "POST"])
-def collections_list(request):
-    if request.method == "GET":
-        queryset = Collection.objects.annotate(products_count=Count("products"))
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(products_count=Count("products"))
+    serializer_class = CollectionSerializer
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def collection_detail(request, pk):
-    collection = get_object_or_404(
-        Collection.objects.annotate(products_count=Count("products")), pk=pk
-    )
-    if request.method == "GET":
-        serializer = CollectionSerializer(collection)
-        return Response(serializer.data)
-    elif request.method == "PUT":
-        serializer = CollectionSerializer(collection, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    if request.method == "DELETE":
+class CollectionDetail(RetrieveUpdateDestroyAPIView):
+    serializer_class = CollectionSerializer
+    queryset = Collection.objects.annotate(products_count=Count("products"))
+
+    def delete(self, request):
+        collection = self.get_object()
         if collection.products.count() > 0:
             return Response(
                 {
